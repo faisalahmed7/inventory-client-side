4509ef975128dad3c0018a73cc537993cfef3009
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-05-07 00:19:30 +0600

    Redirected implementation completed

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,14 +1,20 @@
 
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 import CustomActive from '../CustomActive/CustomActive';
 
 import './Header.css'
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    const handleSignOut = () => {
+        signOut(auth);
+    }
 
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" sticky='top' variant="dark">
@@ -24,9 +30,12 @@ const Header = () => {
                         <Nav.Link as={CustomActive} to="/home"><span>Home</span></Nav.Link>
                         <Nav.Link as={CustomActive} to="/blogs"><span>Blogs</span></Nav.Link>
 
-                        <Nav.Link as={Link} to='/login'>
-                            <span>Login</span>
-                        </Nav.Link>
+                        {
+                            user ?
+                                <button className='btn btn-link  text-white text-decoration-none' onClick={handleSignOut}><span>Log Out</span></button>
+                                : <Nav.Link as={Link} to='/login'>
+                                    <span>Login</span>
+                                </Nav.Link>}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
